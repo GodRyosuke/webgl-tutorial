11c3fdd686258fccef0342906c31fcf426bb86f9
GodRyosuke <ienvs0828@outlook.jp>
2022-12-13 07:31:43 +0900

    fixed

--- src/project/shader/vertex.glsl
+++ src/project/shader/vertex.glsl
@@ -1,6 +1,7 @@
 precision mediump float;
 
 attribute vec3 vertex_position;
+attribute vec2 tex_coord;
 
 // transformations in world-space, like translating an object
 // Without the model matrix, all objects would remain at the origin (0,0,0)
@@ -14,11 +15,20 @@ uniform mat4 view_matrix;
 // Without the projection matrix, the world would be viewed orthographically
 uniform mat4 projection_matrix;
 
+uniform mat4 ModelTransform;
+uniform mat4 gSpriteViewProj;
+
 varying vec3 fragment_position;
+varying vec2 vTextureCoord;
+
 
 void main() {
     fragment_position = vertex_position;
-
     mat4 mvp = projection_matrix * view_matrix * model_matrix;
+
+    vec4 pos = vec4(vertex_position, 1.0);
+	gl_Position = gSpriteViewProj * ModelTransform * pos;
+    vTextureCoord = tex_coord;
+    
     gl_Position = mvp * vec4(vertex_position, 1);
 }
